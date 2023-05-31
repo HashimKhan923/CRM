e7d99ceeb2f138cabca6e3dd85f0cdff89c632d3
HashimKhan923 <khanhash1994@gmail.com>
2023-05-31 03:27:20 -0700

    attendence

--- app/Http/Controllers/Admin/AttendenceController.php
+++ app/Http/Controllers/Admin/AttendenceController.php
@@ -10,8 +10,13 @@ class AttendenceController extends Controller
 {
     public function index()
     {
-        $Attendence = Time::with('user')->whereDate('created_at',Carbon::today());
+        $Attendence = Time::with('user')->whereDate('created_at',Carbon::today())->get();
 
         return response()->json(['Attendence'=>$Attendence]);
+    }
+
+    public function search()
+    {
+        $data = Time::with('user')->where('created_at','>=',$request->from_date)->where('created_at','<=',$request->to_date)->get();
     }
 }
