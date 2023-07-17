1ddc01d69c6d2c6849fc6c007967ace710b9b5c7
HashimKhan923 <khanhash1994@gmail.com>
2023-07-17 19:11:06 +0500

    dsdf

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Department;
+use App\Models\Shift;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $UserCount = User::count();
+        $DepartmentCount = Department::count();
+        $ShiftCount = Shift::count();
+
+        return redirect()->json(['UserCount'=>$UserCount,'DepartmentCount'=>$DepartmentCount,'ShiftCount'=>$ShiftCount]);
+    }
+}
