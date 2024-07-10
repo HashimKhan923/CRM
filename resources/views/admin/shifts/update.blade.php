0f6cfa8426b8ad85791a744c116f51060fe74afb
HashimKhan923 <khanhash1994@gmail.com>
2024-07-10 20:56:29 +0500

    wewewded

--- resources/views/admin/shifts/update.blade.php
+++ resources/views/admin/shifts/update.blade.php
@@ -23,7 +23,8 @@
                                                 <div class="text-danger">{{ $message }}</div>
                                             @enderror
                                             </div>
-                                        
+                                        @php $data->time_from = \Carbon\Carbon::parse($data->time_from)->format('H:i');
+$data->time_to = \Carbon\Carbon::parse($data->time_to)->format('H:i'); @endphp
 
                                         <div class="form-group col-md-4">
                                             <label for="">From</label>
